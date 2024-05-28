6a84631d5adcc15a62829e32eab8e500dc9ea801
Karan Balkar <karanbalkar@gmail.com>
2024-05-28 17:41:18 +0530

    initial commit

---
+++ src/main/java/com/redvinca/assignment/ecom_backend/EcomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.redvinca.assignment.ecom_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redvinca/assignment/ecom_backend/EcomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redvinca.assignment.ecom_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
