77394e20fcd6730446f42827cedf5d6eff5bb975
shwetapanchal09 <05panchalshweta@gmail.com>
2024-06-03 11:29:03 +0530

    QuantityWarningMessage

--- src/main/java/com/redvinca/assignment/ecom_backend/controller/CartController.java
+++ src/main/java/com/redvinca/assignment/ecom_backend/controller/CartController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.redvinca.assignment.ecom_backend.exception.CartNotFoundException;
+import com.redvinca.assignment.ecom_backend.exception.InsufficientStockException;
+import com.redvinca.assignment.ecom_backend.exception.NegativeQuantityException;
 import com.redvinca.assignment.ecom_backend.model.Cart;
 import com.redvinca.assignment.ecom_backend.request.DeleteItemToCartRequest;
 import com.redvinca.assignment.ecom_backend.request.UpdateQuanatityRequest;
@@ -42,14 +45,32 @@ public class CartController {
 		}
 	}
 
+	// this api is deleted by Shweta
+//	public ResponseEntity<?> updateCartQuantity(@RequestParam Long cartId, @RequestParam int quantity) {
+//		try {
+//			Cart updatedCart = cartService.updateCartQuantity(cartId, quantity);
+//			return ResponseEntity.ok(updatedCart);
+//		} catch (Exception e) {
+//			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to update cart item quantity");
+//		}
+//	}
+
 	@PutMapping("/update")
 	public ResponseEntity<?> updateCartQuantity(@RequestParam Long cartId, @RequestParam int quantity) {
 		try {
 			Cart updatedCart = cartService.updateCartQuantity(cartId, quantity);
 			return ResponseEntity.ok(updatedCart);
+		} catch (CartNotFoundException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		} catch (InsufficientStockException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+		} catch (NegativeQuantityException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
 		} catch (Exception e) {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to update cart item quantity");
+			// Handle unexpected exceptions
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
 		}
+
 	}
 
 	@DeleteMapping("/remove")
--- src/main/java/com/redvinca/assignment/ecom_backend/entity/CartList.java
+++ src/main/java/com/redvinca/assignment/ecom_backend/entity/CartList.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Data
 @NoArgsConstructor
